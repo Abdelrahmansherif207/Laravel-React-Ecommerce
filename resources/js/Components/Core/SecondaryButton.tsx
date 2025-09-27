ba7bedc53c0ae23198bbe72cf2ab7977d1abcc09
Abdelrahman Sherif <dodger@Abdelrahmans-MacBook-Pro.local>
2025-09-27 13:58:07 +0300

    Initial Commit

---
+++ resources/js/Components/Core/SecondaryButton.tsx
@@ -0,0 +1,20 @@
+import {ButtonHTMLAttributes} from 'react';
+
+export default function SecondaryButton(
+  {
+    type = 'button',
+    className = '',
+    disabled,
+    children,
+    ...props
+  }: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className={`btn ${className || ""}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
